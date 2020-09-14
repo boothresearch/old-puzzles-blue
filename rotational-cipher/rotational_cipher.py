551e10b019609228eb7f305fb49bf8fc2f8ce94d
nwuthenow <nicolas.wuthenow@gmail.com>
2020-09-14 21:19:35 +0000

    cipher_task_w_bug_not_finished

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,30 @@
 def rotate(text, key):
-    pass
+    out = ""
+
+    for l in text:
+        if l.isalpha():
+            new_l = ""
+            if l.isupper():
+                if ord(l) + key <= ord('Z'):
+                    new_l += chr(ord(l) + key)
+                    print("1")
+                elif ord(l) + key >= ord('Z'):
+                    new_l += chr(ord(l) - key)
+                    print("2")
+            elif l.islower():
+                if ord(l) + key <= ord('z'):
+                    new_l += chr(ord(l) + key)
+                    print("3")
+                elif ord(l) + key >= ord('z'):
+                    new_l += chr(ord(l) - key)
+                    print("4")
+
+            out += new_l 
+
+        else:
+            out += l
+    
+    print(out)
+
+
+rotate("Zet's 12 Abc", 21)
