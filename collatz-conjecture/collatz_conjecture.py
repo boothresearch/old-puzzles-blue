f2102b2acfe82aa877be3dd0d2be3da5f8680560
nwuthenow <nicolas.wuthenow@gmail.com>
2020-09-14 18:54:10 +0000

    solution to collatz

--- collatz-conjecture/collatz_conjecture.py
+++ collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,14 @@
 def steps(number):
-    pass
+    n = number
+    i = 0
+
+    while n != 1:
+        if (n % 2) == 0:
+            n = n / 2
+        else:
+            n = 3 * n + 1
+        i = i + 1
+
+    if n == 1 :
+        # print("You reached 1 in", i, "steps")
+        return i
