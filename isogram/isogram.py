db5953d79259d0ff0db7a0cbfa896cd1a508601a
harleenie <harleen.kaur@chicagobooth.edu>
2020-09-15 15:32:12 +0000

    isogram code

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -15,3 +15,24 @@ Examples of isograms include:
 The word *isograms*, however, is not an isogram, because the s repeats.
 """
 
+# Edge case when s is of length 0 or 1
+if len(s) in [0, 1]:
+    return True
+
+assert type(s) == str
+# Preprocess the string
+processed_string = s.replace(' ', '')
+ processed_string = processed_string.replace('-', '')
+processed_string = processed_string.lower()
+
+# Proprocessing done
+assert processed_string.find(' ') == -1
+assert processed_string.find('-') == -1
+assert processed_string.islower() == True
+
+# Make letters distinct
+str_set = ''.join(set(list(processed_string)))
+
+# Check len(set(str)) == len(str)
+# Isogram if True, not isogram if False
+return len(str_set) == len(processed_string)
