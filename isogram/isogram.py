385bbd471a5bb23f3a8199a96773968be946fad2
harleenie <harleen.kaur@chicagobooth.edu>
2020-09-15 15:25:05 +0000

    definition of isogram and description of program

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,17 @@
 def is_isogram(string):
     pass
+"""
+Program to determine whether a word or phrase is an isogram.
+An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter, 
+however spaces and hyphens are allowed to appear multiple times.
+
+Examples of isograms include:
+
+    lumberjacks
+    background
+    downstream
+    six-year-old
+
+The word *isograms*, however, is not an isogram, because the s repeats.
+"""
+
